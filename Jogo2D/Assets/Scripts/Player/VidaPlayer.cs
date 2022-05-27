d8df93749b0c241863f2357e87f304e1f3328c2b
Arthur De Souza Silva <105186158+ArthurSilv4@users.noreply.github.com>
2022-05-26 21:35:30 -0300

    main

---
+++ Jogo2D/Assets/Scripts/Player/VidaPlayer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class VidaPlayer : MonoBehaviour
+{
+    public static int maxVida = 3;
+    public static int currenteVida = 1;
+}
